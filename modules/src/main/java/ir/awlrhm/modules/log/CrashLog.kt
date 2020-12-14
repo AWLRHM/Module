a5c0c7b18cb2200f6307bbebf95f378444005096
Nikafarinegan Rayaneh <nikafarineganrayaneh@gmail.com>
2020-12-14 14:28:08 +0330

    add copy to clipboard function

--- modules/src/main/java/ir/awlrhm/modules/log/CrashLog.kt
+++ modules/src/main/java/ir/awlrhm/modules/log/CrashLog.kt
@@ -1,16 +1,23 @@
 package ir.awlrhm.modules.log
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.FragmentActivity
+import ir.awlrhm.modules.extensions.yToast
 import ir.awlrhm.modules.log.database.AWLRHMDatabaseHelper
 import ir.awlrhm.modules.log.investigation.Crash
 import ir.awlrhm.modules.log.viewmodel.CrashViewModel
 import ir.awlrhm.modules.view.ActionDialog
+import ir.awrhm.modules.enums.MessageStatus
 
 class CrashLog(
     private val activity: FragmentActivity
 ){
     private val database = AWLRHMDatabaseHelper(activity)
     private val crashes = database.crashes
+    private val log = StringBuilder()
 
     fun isExistCrashLog(): Boolean {
         return !crashes[0].stackTrace.isNullOrEmpty()
@@ -19,21 +26,28 @@ class CrashLog(
     val crashLogs: List<Crash>
         get() = crashes
 
+   fun copyToClipboard(){
+        val clipboard: ClipboardManager =
+            activity.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip = ClipData.newPlainText("Stack trace", log.toString())
+        clipboard.setPrimaryClip(clip)
+       activity.yToast("Copy to Clipboard", MessageStatus.INFORMATION)
+    }
+
     fun showCrashLog() {
         val viewModel = CrashViewModel(crashes[0])
-        val log = StringBuilder()
-        log.append("stack trace: \n")
-        log.append(crashes[0].stackTrace)
-        log.append("\n\n")
-        log.append("place of crash : \n")
-        log.append(crashes[0].placeOfCrash)
-        log.append("\n\n")
-        log.append("reason of crash : \n")
-        log.append(crashes[0].reasonOfCrash)
-        log.append("\n\n")
         log.append("device info : ${viewModel.deviceName}\n")
         log.append("device brand : ${viewModel.deviceBrand}\n")
-        log.append("android version : ${viewModel.deviceAndroidApiVersion}")
+        log.append("android version : ${viewModel.deviceAndroidApiVersion}\n")
+        log.append("\n")
+        log.append("Place of Crash ==================\n")
+        log.append(crashes[0].placeOfCrash)
+        log.append("\n")
+        log.append("Reason of Crash ==================\n")
+        log.append(crashes[0].reasonOfCrash)
+        log.append("\n")
+        log.append("Stack Trace ==================\n")
+        log.append(crashes[0].stackTrace)
 
         ActionDialog.Builder()
             .description(log.toString())
