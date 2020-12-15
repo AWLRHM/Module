447eaeaaa2a406b08cfea3e387b1e3e0c89e4fb5
Nikafarinegan Rayaneh <nikafarineganrayaneh@gmail.com>
2020-12-15 09:35:09 +0330

    add check security function to module

--- modules/src/main/java/ir/awlrhm/modules/extensions/FragmentActivity.kt
+++ modules/src/main/java/ir/awlrhm/modules/extensions/FragmentActivity.kt
@@ -1,6 +1,10 @@
 package ir.awlrhm.modules.extensions
 
 import androidx.fragment.app.FragmentActivity
+import ir.awlrhm.modules.security.CheckEmulatorUtil
+import ir.awlrhm.modules.security.CheckReverseEngineeringToolsUtil
+import ir.awlrhm.modules.security.CheckRootUtil
+import ir.awlrhm.modules.security.CloneApp
 import ir.awlrhm.modules.view.ActionDialog
 import ir.awlrhm.modules.view.downloadVersion.DownloadNewVersion
 import ir.awlrhm.modules.view.downloadVersion.OnDownloadListener
@@ -8,19 +12,6 @@ import ir.awlrhm.modules.view.progress.ProgressDialog
 import ir.awrhm.modules.R
 import ir.awrhm.modules.enums.MessageStatus
 import kotlin.system.exitProcess
-
-fun FragmentActivity.showSecurityErrorDialog(title: String, message: String) {
-    ActionDialog.Builder()
-        .action(MessageStatus.ERROR)
-        .title(if (title.isEmpty()) getString(R.string.error) else title)
-        .description(if (message.isEmpty()) getString(R.string.security_problem) else message)
-        .negative(getString(R.string.ok)) {
-            exitProcess(0)
-        }
-        .build()
-        .show(supportFragmentManager, ActionDialog.TAG)
-}
-
 
 fun FragmentActivity.downloadNewVersion(
     downloadUrl: String?,
@@ -56,4 +47,47 @@ fun FragmentActivity.downloadNewVersion(
             }
         }
     ).execute()
+}
+
+fun FragmentActivity.isSecure(callback: ()->Unit) {
+    val isRooted = CheckRootUtil().isDeviceRooted
+    val isEmulator = CheckEmulatorUtil().isEmulator(this)
+    val isExistReverseEngTools =
+        CheckReverseEngineeringToolsUtil().isExistTools(this)
+    val isCloneAppInstalled = CloneApp().isInstalled(this)
+
+    var isSecure = false
+    var title = ""
+    var message = ""
+    when {
+        isEmulator -> {
+            title = getString(R.string.emulator_detection)
+            message = getString(R.string.app_not_run_on_emulator)
+        }
+        isRooted -> {
+            title = getString(R.string.root_detection)
+            message = getString(R.string.your_device_is_root)
+        }
+        isExistReverseEngTools -> {
+            title = getString(R.string.reverse_engineering_tools_detection)
+            message = getString(R.string.is_exist_reverse_engineering_tools)
+        }
+        isCloneAppInstalled -> {
+            title = getString(R.string.parallel_space_app_exist)
+            message = getString(R.string.is_exist_parallel_space_app)
+        }
+        else -> isSecure = true
+    }
+    if(isSecure)
+        callback.invoke()
+    else
+        ActionDialog.Builder()
+            .action(MessageStatus.ERROR)
+            .title(if (title.isEmpty()) getString(R.string.error) else title)
+            .description(if (message.isEmpty()) getString(R.string.security_problem) else message)
+            .negative(getString(R.string.ok)) {
+                exitProcess(0)
+            }
+            .build()
+            .show(supportFragmentManager, ActionDialog.TAG)
 }
