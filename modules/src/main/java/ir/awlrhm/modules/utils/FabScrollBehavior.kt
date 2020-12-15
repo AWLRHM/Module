dc1ec11f013a110d004025f214aa5200013852b6
Nikafarinegan Rayaneh <nikafarineganrayaneh@gmail.com>
2020-12-15 10:29:45 +0330

    add fab behavior

---
+++ modules/src/main/java/ir/awlrhm/modules/utils/FabScrollBehavior.kt
@@ -0,0 +1,42 @@
+package ir.awlrhm.modules.utils
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class FabScrollBehavior(context: Context, attrs: AttributeSet): CoordinatorLayout.Behavior<FloatingActionButton> (context, attrs) {
+
+    override fun onStartNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: FloatingActionButton,
+        directTargetChild: View,
+        target: View,
+        nestedScrollAxes: Int
+    ): Boolean {
+        return true
+    }
+
+    override fun onNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: FloatingActionButton,
+        target: View,
+        dxConsumed: Int,
+        dyConsumed: Int,
+        dxUnconsumed: Int,
+        dyUnconsumed: Int,
+        type: Int
+    ) {
+        super.onNestedScroll(
+            coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed,
+            dyUnconsumed, type
+        )
+
+        if (dyConsumed > 0 && child.visibility == View.VISIBLE) {
+            child.hide()
+        } else if (dyConsumed < 0 && child.visibility != View.VISIBLE) {
+            child.show()
+        }
+    }
+}
