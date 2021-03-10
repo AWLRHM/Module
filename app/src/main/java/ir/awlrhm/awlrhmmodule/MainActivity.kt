5e0ea55c6a9810105baa06b4817dc9283b9a29a0
Nikafarinegan Rayaneh <nikafarineganrayaneh@gmail.com>
2021-03-10 12:21:38 +0330

    fix bug

--- app/src/main/java/ir/awlrhm/awlrhmmodule/MainActivity.kt
+++ app/src/main/java/ir/awlrhm/awlrhmmodule/MainActivity.kt
@@ -2,6 +2,7 @@ package ir.awlrhm.awlrhmmodule
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import ir.awlrhm.modules.extensions.showDatePicker
 import ir.awrhm.awlrhmmodule.R
@@ -9,14 +10,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var paging = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
       spinner.setOnClickListener { spinner.loading(true) }
         rclItem
-            .layoutManager(LinearLayoutManager(this))
+            .layoutManager(GridLayoutManager(this,2))
             .theme(R.color.pink_500)
 
         rclItem.view?.adapter = Adapter(
